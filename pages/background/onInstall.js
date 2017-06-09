ed68147110ac359ce527b6dbe2600bd59647831d
Manak <manak.kapoor@holmusk.com>
2017-06-09 13:22:05 -0400

    Updated to attempt to fix random background change failures

--- pages/background/onInstall.js
+++ pages/background/onInstall.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function(){
 	//Refresh wallpaper alarm (every hour)
-	chrome.alarms.create("refresh-wallpaper", {when:Date.now(), periodInMinutes:60});
-	chrome.alarms.create("refresh-weather", {when:Date.now(), periodInMinutes:60});
+	chrome.alarms.create("refresh-wallpaper", {when:Date.now()+1000, periodInMinutes:60});
+	chrome.alarms.create("refresh-weather", {when:Date.now()+1000, periodInMinutes:60});
 
 });
