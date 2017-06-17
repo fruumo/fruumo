a982947618af7cb81f583dac99d7214b28556450
Manak <manak.kapoor@holmusk.com>
2017-06-17 17:37:55 -0400

    Webpack now compiles a seperate CSS file

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
 	module: {
 		rules: [
-		  {
-		    test: /\.(sass|scss)$/,
-		    use: [
-		      'style-loader',
-		      'css-loader',
-		      'sass-loader',
-		    ]
-		  }
-		],
+		{
+	        test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				fallback: "style-loader",
+				use: ['css-loader', 'sass-loader']
+			})
+		}
+		]
 	},
+	plugins: [
+		new ExtractTextPlugin("./bundle.css"),
+	],
 	context: path.resolve(__dirname, './pages/'),
 	entry: {
-	index: './index/js/index.js',
+		index: './index/js/index.js',
 	},
 	output: {
-	path: path.resolve(__dirname, './dist/'),
-	filename: '[name].bundle.js',
+		path: path.resolve(__dirname, './dist/'),
+		filename: '[name].bundle.js',
 	},
 };
+
