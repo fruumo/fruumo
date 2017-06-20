d90ddcf3946f37af8cbd1e2a999932c31ce2eb86
Manak <manak.kapoor@holmusk.com>
2017-06-20 18:38:52 -0400

    Shortened the weather update time to 10 minutes

--- pages/background/onInstall.js
+++ pages/background/onInstall.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(function(){
 	//Refresh wallpaper alarm (every hour)
 	chrome.alarms.create("refresh-wallpaper", {when:Date.now()+1000, periodInMinutes:60});
-	chrome.alarms.create("refresh-weather", {when:Date.now()+1000, periodInMinutes:60});
+	chrome.alarms.create("refresh-weather", {when:Date.now()+1000, periodInMinutes:10});
 
 	//Defaults
 	localStorage.searchDomain = "https://www.google.ca/search?q="
