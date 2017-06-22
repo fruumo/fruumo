a1e653b79b7434ba997fbdee5e31af81bb13120b
Manak <manak.kapoor@holmusk.com>
2017-06-22 15:46:30 -0400

    Added transition to focus mode

--- pages/index/js/core/focus/focus.js
+++ pages/index/js/core/focus/focus.js
@@ -2,16 +2,20 @@ var mousetrap = require('mousetrap');
 
 module.exports = {
 	name:'focus-mode',
-	DOM:['.topsites-container','.search-container', '.time-container','.ticker-container'],
+	DOM:['.topsites-container','.search-container', '.time-container','.ticker-container','.container'],
 	preload: function(){
 		return new Promise(function(resolve, reject){
 			chrome.storage.sync.get("settingFocus", function(storage){
 
 				if(storage.settingFocus){
-					this.DOM[3][0].className += " focus";
-					this.DOM[2][0].className += " focus";
-					this.DOM[1][0].className += " focus";
-					this.DOM[0][0].className += " focus";
+					this.DOM[4][0].style.opacity = "0";
+					setTimeout(function(){
+						this.DOM[3][0].className += " focus";
+						this.DOM[2][0].className += " focus";
+						this.DOM[1][0].className += " focus";
+						this.DOM[0][0].className += " focus";
+						this.DOM[4][0].style.opacity = "1";
+					}.bind(this),300);
 				}
 
 				//dynamic settings
@@ -23,15 +27,23 @@ module.exports = {
 					if(changes.settingFocus.newValue == undefined)
 						return;
 					if(changes.settingFocus.newValue){
-						this.DOM[0][0].className += " focus";
-						this.DOM[1][0].className += " focus";
-						this.DOM[2][0].className += " focus";
-						this.DOM[3][0].className += " focus";
+						this.DOM[4][0].style.opacity = "0";
+						setTimeout(function(){
+							this.DOM[3][0].className += " focus";
+							this.DOM[2][0].className += " focus";
+							this.DOM[1][0].className += " focus";
+							this.DOM[0][0].className += " focus";
+							this.DOM[4][0].style.opacity = "1";
+						}.bind(this),300);
 					} else {
-						this.DOM[3][0].className = this.DOM[3][0].className.replace(" focus", "");
-						this.DOM[2][0].className = this.DOM[2][0].className.replace(" focus", "");
-						this.DOM[1][0].className = this.DOM[1][0].className.replace(" focus", "");
-						this.DOM[0][0].className = this.DOM[0][0].className.replace(" focus", "");
+						this.DOM[4][0].style.opacity = "0";
+						setTimeout(function(){
+							this.DOM[3][0].className = this.DOM[3][0].className.replace(" focus", "");
+							this.DOM[2][0].className = this.DOM[2][0].className.replace(" focus", "");
+							this.DOM[1][0].className = this.DOM[1][0].className.replace(" focus", "");
+							this.DOM[0][0].className = this.DOM[0][0].className.replace(" focus", "");
+							this.DOM[4][0].style.opacity = "1";
+						}.bind(this),300);
 					}
 				}.bind(this));
 
