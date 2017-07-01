dbee518a566e1e6980bb5a5754df4bb175f7d266
Manak <manak.kapoor@holmusk.com>
2017-07-01 15:14:04 -0400

    Added custom wallpapers

--- pages/background/wallpaper.js
+++ pages/background/wallpaper.js
@@ -6,37 +6,41 @@ chrome.alarms.onAlarm.addListener(function(alarm){
 	if(alarm.name != "refresh-wallpaper" ){
 		return;
 	}
-	console.log('refreshing wallpaper...');
-	var max = 0;
-	var min = 24;
-	var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-	fetch("http://fruumo.com/wallpaper/"+randomNumber+".json?r="+Math.random())
-	.then(function(response){
-		if(response.ok)
-			return response.json();
-	})
-	.then(function(data){
-		//Get new wallpaper data
-		//regular or full
-		fetch("https://source.unsplash.com/"+data.id+"/"+screen.width+"x"+screen.height)
+	chrome.storage.local.get({settingCustomWallpaper:false}, function(storage){
+		if(storage.settingCustomWallpaper)
+			return;
+		console.log('refreshing wallpaper...');
+		var max = 0;
+		var min = 24;
+		var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+		fetch("http://fruumo.com/wallpaper/"+randomNumber+".json?r="+Math.random())
 		.then(function(response){
 			if(response.ok)
-				return response.blob();
+				return response.json();
 		})
-		.then(function(blob){
-			var reader = new FileReader();
-			reader.onload = function() {
-		    	var dataUrl = reader.result;
-		    	var rgb = hexToRgb(data.color);
-		    	var luminance = 1 - (((0.299*rgb.r) + (0.587*rgb.g) + (0.114*rgb.b))/255);
-		    	var fontColor = "";
-		    	if(luminance < 0.5)
-		    		fontColor = "#000";
-		    	else
-		    		fontColor = "#FFF";
-				chrome.storage.local.set({wallpaper:{image:dataUrl,author:data.user,location:data.location, color:data.color,fontColor:fontColor,luminance:luminance}});
-		    };
-	        reader.readAsDataURL(blob);
+		.then(function(data){
+			//Get new wallpaper data
+			//regular or full
+			fetch("https://source.unsplash.com/"+data.id+"/"+screen.width+"x"+screen.height)
+			.then(function(response){
+				if(response.ok)
+					return response.blob();
+			})
+			.then(function(blob){
+				var reader = new FileReader();
+				reader.onload = function() {
+					var dataUrl = reader.result;
+					var rgb = hexToRgb(data.color);
+					var luminance = 1 - (((0.299*rgb.r) + (0.587*rgb.g) + (0.114*rgb.b))/255);
+					var fontColor = "";
+					//if(luminance < 0.5)
+					//	fontColor = "#000";
+					//else
+						fontColor = "#FFF";
+					chrome.storage.local.set({wallpaper:{image:dataUrl,author:data.user,location:data.location, color:data.color,fontColor:fontColor,luminance:luminance}});
+				};
+				reader.readAsDataURL(blob);
+			});
 		});
 	});
 });
--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,45 +1,79 @@
 require('./index.scss');
-chrome.storage.sync.get(null, function(storage){
-	console.log(storage);
-	var displayTopsites = document.getElementById("display-topsites");
-	var time = document.getElementById("24h");
-	var focusMode = document.getElementById("focus-mode");
-	var reset = document.getElementById("reset");
-	var metricWeather = document.getElementById("metric-weather");
+chrome.storage.local.get(null, function(lstorage){
 
-	if(storage.settingDisplayTopsites){
-		displayTopsites.checked = true;
-	}
-	if(storage.settingIs24h){
-		time.checked = true;
-	}
-	if(storage.settingFocus){
-		focusMode.checked = true;
-	}
-	if(storage.settingMetric || storage.settingMetric == undefined){
-		metricWeather.checked = true;
-	}
-	metricWeather.addEventListener("click", function(){
-		chrome.storage.sync.set({settingMetric:this.checked});
-	});
+	chrome.storage.sync.get(null, function(storage){
+		console.log(storage);
+		var displayTopsites = document.getElementById("display-topsites");
+		var time = document.getElementById("24h");
+		var focusMode = document.getElementById("focus-mode");
+		var reset = document.getElementById("reset");
+		var metricWeather = document.getElementById("metric-weather");
+		var customWallpaperBrowse = document.getElementById("custom-wallpaper-browse");
+		var customWallpaper = document.getElementById("custom-wallpaper");
+		var fruumoWallpaper = document.getElementById("fruumo-wallpaper");
+		if(storage.settingDisplayTopsites){
+			displayTopsites.checked = true;
+		}
+		if(storage.settingIs24h){
+			time.checked = true;
+		}
+		if(storage.settingFocus){
+			focusMode.checked = true;
+		}
+		if(storage.settingMetric || storage.settingMetric == undefined){
+			metricWeather.checked = true;
+		}
+		if(!lstorage.settingCustomWallpaper){
+			fruumoWallpaper.checked = true;
+		} else {
+			customWallpaper.checked = true;
+		}
 
-	displayTopsites.addEventListener("click", function(){
-		chrome.storage.sync.set({settingDisplayTopsites:this.checked});
-	});
+		customWallpaperBrowse.addEventListener("change", function(e){
+			if(e.target.files.length == 0)
+				return;
 
-	time.addEventListener("click", function(){
-		chrome.storage.sync.set({settingIs24h:this.checked});
-	});
+    		var file = e.target.files[0];
+    		 if (!file.type.match('image.*')) {
+       			return;
+      		}
+      		var reader = new FileReader();
+      		reader.onload = function(){
+				var dataUrl = reader.result;
+				var fontColor = "#FFF";
+				chrome.storage.local.set({wallpaper:{image:dataUrl,author:"",location:"", color:"",fontColor:fontColor,luminance:0}});
+				chrome.storage.local.set({settingCustomWallpaper:true});
+      		};
+      		reader.readAsDataURL(file);
+		});
+		customWallpaper.addEventListener("click", function(){
+			customWallpaperBrowse.click();
+		});
+		fruumoWallpaper.addEventListener("click", function(){
+			chrome.storage.local.remove("settingCustomWallpaper");
+			chrome.alarms.create("refresh-wallpaper", {when:Date.now()+1000, periodInMinutes:60});
+		});
+		metricWeather.addEventListener("click", function(){
+			chrome.storage.sync.set({settingMetric:this.checked});
+		});
 
-	focusMode.addEventListener("click", function(){
-		chrome.storage.sync.set({settingFocus:this.checked});
-	});
+		displayTopsites.addEventListener("click", function(){
+			chrome.storage.sync.set({settingDisplayTopsites:this.checked});
+		});
 
-	reset.addEventListener("click", function(){
-		chrome.storage.sync.set({settingsReset:true}, function(){
-			location.reload();
+		time.addEventListener("click", function(){
+			chrome.storage.sync.set({settingIs24h:this.checked});
+		});
+
+		focusMode.addEventListener("click", function(){
+			chrome.storage.sync.set({settingFocus:this.checked});
+		});
+
+		reset.addEventListener("click", function(){
+			chrome.storage.sync.set({settingsReset:true}, function(){
+				location.reload();
+			});
 		});
 	});
 });
-
 require('./weather.js');
