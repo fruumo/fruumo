b807d2debff171be416a25496e2f514bea809944
Manak <manak.kapoor@holmusk.com>
2017-07-04 12:10:12 -0400

    Added return to fix weird bug

--- pages/index/js/core/search/search.js
+++ pages/index/js/core/search/search.js
@@ -199,8 +199,10 @@ module.exports = {
     	}
 	},
 	launchResult: function(e){
-		if(this.DOM[1][0].children.length == 0 && this.DOM[0][0].value != "")
+		if(this.DOM[1][0].children.length == 0 && this.DOM[0][0].value != ""){
 			window.top.location = localStorage.searchDomain + this.DOM[0][0].value;
+			return;
+		}
 		if(this.keyboardSelectedResult == -1){
 			window.top.location = localStorage.searchDomain + this.DOM[0][0].value;
 			return;
