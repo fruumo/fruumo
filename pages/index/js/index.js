87c0f581210d77e36fe38f1d0271bf5bd4154d26
Manak <manak.kapoor@holmusk.com>
2017-07-05 08:15:30 -0400

    changed to async analytics

--- pages/index/js/index.js
+++ pages/index/js/index.js
@@ -34,7 +34,10 @@ window.onload = function(){
 			}	
 		}	
 	});
-
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-101959257-1', 'auto');
+	ga('set', 'checkProtocolTask', function(){});
+	ga('send', 'pageview');
   	ga('send', 'event', 'Fruumo', 'load', appVersion);
 	//Log page load time
 	setTimeout(function(){
