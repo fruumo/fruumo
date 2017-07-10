edc08736946f8fe91b0e10f1cd29317e624d8862
Manak <manak.kapoor@holmusk.com>
2017-07-09 23:09:21 -0400

    Updated notification page is now personalized with name

--- pages/updated/index.js
+++ pages/updated/index.js
@@ -1,6 +1,7 @@
 require('./index.scss');
 
 document.getElementById("app-version").innerText = chrome.app.getDetails().version;
+document.getElementById("username").innerText = (localStorage.username == "" || !localStorage.username? " there": localStorage.username);
 var updateList = document.getElementById("update-list");
 var updates = JSON.parse(localStorage.updateScreen);
 for(var i in updates){
