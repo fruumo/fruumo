59fa770a654ae97348b36f021053ac130c25247c
Manak <manak.kapoor@gmail.com>
2018-03-23 21:28:26 -0400

    Message for indexing

--- pages/background/scripts/search-engine/search.js
+++ pages/background/scripts/search-engine/search.js
@@ -1,42 +1,53 @@
-var fLink = 100;
-var fTyped = 150;
-var fAutoBookmark = 75;
-var fReload,fStartPage,fFormSubmit,fKeyword,fGenerated = 0;
-var fCutoff1 = 4;
-var fCutoff2 = 14;
-var fCutoff3 = 31;
-var fCutoff4 = 90;
-var fWeight1 = 100;
-var fWeight2 = 70;
-var fWeight3 = 50;
-var fWeight4 = 30;
-var fWeight5 = 10;
-module.exports = {
-	name:"sem",
-	db: undefined,
-	openDb: function(callback) {
-		if (!this.db) {
-			this.db = openDatabase('fruumodb', '1.0', 'Fruumo data', 100 * 1024 * 1024);
-			this.db.transaction(function(tx){
-				console.log("Creating database tables");
-				tx.executeSql(`CREATE TABLE IF NOT EXISTS urls (
-					url TEXT, hostname TEXT, type NUMERIC,
-					title TEXT, frecency NUMERIC DEFAULT -1,
+/*
+ 	To do:
+ 		- Rewrite frecency algorithm
+ 		- Handle bookmarks removal addition
+ 		- Handle reseting database function
+ 		- Frecency is not handling bookmark views properly
+ 		- Fallback if indexing failure
+ 		- Show indexing message when indexing
+ 		*/
+ 		var fLink = 100;
+ 		var fTyped = 150;
+ 		var fAutoBookmark = 75;
+ 		var fReload,fStartPage,fFormSubmit,fKeyword,fGenerated = 0;
+ 		var fCutoff1 = 4;
+ 		var fCutoff2 = 14;
+ 		var fCutoff3 = 31;
+ 		var fCutoff4 = 90;
+ 		var fWeight1 = 100;
+ 		var fWeight2 = 70;
+ 		var fWeight3 = 50;
+ 		var fWeight4 = 30;
+ 		var fWeight5 = 10;
+ 		module.exports = {
+ 			name:"sem",
+ 			db: undefined,
+ 			indexCount:0,
+ 			openDb: function(callback, force) {
+ 				if (!this.db || force) {
+ 					this.db = openDatabase('fruumodb', '1.0', 'Fruumo data', 100 * 1024 * 1024);
+ 					this.db.transaction(function(tx){
+ 						if(!force)
+ 							console.log("Creating database tables");
+ 						tx.executeSql(`CREATE TABLE IF NOT EXISTS urls (
+ 							url TEXT, hostname TEXT, type NUMERIC,
+ 							title TEXT, frecency NUMERIC DEFAULT -1,
 					id NUMERIC DEFAULT 0)`); // type1 = history item, type2 = bookmark
-				tx.executeSql('CREATE INDEX IF NOT EXISTS urlindex ON urls (url)');
-				tx.executeSql('CREATE UNIQUE INDEX IF NOT EXISTS hostnameindex ON urls (hostname)');
-				tx.executeSql('CREATE INDEX IF NOT EXISTS titleindex ON urls (title)');
-				tx.executeSql('CREATE INDEX IF NOT EXISTS frecencyindex ON urls (frecency)');
-				tx.executeSql('CREATE INDEX IF NOT EXISTS typeindex ON urls (type)');
-				callback();
-			}.bind(this));
-		} else {
-			callback();
-		}
-	},
-	onload: function(){
-		var self = this;
-		this.openDb(function(){
+ 						tx.executeSql('CREATE INDEX IF NOT EXISTS urlindex ON urls (url)');
+ 						tx.executeSql('CREATE UNIQUE INDEX IF NOT EXISTS hostnameindex ON urls (hostname)');
+ 						tx.executeSql('CREATE INDEX IF NOT EXISTS titleindex ON urls (title)');
+ 						tx.executeSql('CREATE INDEX IF NOT EXISTS frecencyindex ON urls (frecency)');
+ 						tx.executeSql('CREATE INDEX IF NOT EXISTS typeindex ON urls (type)');
+ 						callback();
+ 					}.bind(this));
+ 				} else {
+ 					callback();
+ 				}
+ 			},
+ 			onload: function(){
+ 				var self = this;
+ 				this.openDb(function(){
 			//Count number of items in db and check if indexing is required
 			self.db.transaction(function(tx){
 				tx.executeSql('SELECT count(*) FROM urls',[], function(tx, results){
@@ -47,46 +58,71 @@ module.exports = {
 			});
 		});
 
-		chrome.history.onVisited.addListener(function(historyItem){
-			if(historyItem.url.indexOf("chrome://")!= -1 || historyItem.url.indexOf("chrome-extension://")!= -1)
-				return;
-			self.addItemToIndex(historyItem);
-		}.bind(this));
+ 				chrome.history.onVisited.addListener(function(historyItem){
+ 					if(historyItem.url.indexOf("chrome://")!= -1 || historyItem.url.indexOf("chrome-extension://")!= -1)
+ 						return;
+ 					self.addItemToIndex(historyItem);
+ 				}.bind(this));
 
-		chrome.runtime.onMessage.addListener(function(request,sender, respond){
-			if(request.type != "history-search"){
-				return;
-			}
-			var query = request.data.query;
-			if(query[query.length-1] == " "){
-				return respond([]);
-			}
-			this.db.transaction(function(tx){
-				tx.executeSql('SELECT * FROM urls WHERE ( (hostname LIKE ?) OR (hostname LIKE ?) OR (title LIKE ?) ) ORDER BY frecency DESC LIMIT 4' ,['%'+query+'%','%'+query.replace('www.','')+'%','%'+query+'%'], function(tx, results){
-					var resultSet = [];
-					console.log(resultSet);
-					for(var i=0;i<=results.rows.length-1; i++){
-						resultSet.push(results.rows.item(i));
-						if(resultSet[i].title == ""){
-							resultSet[i].title = resultSet[i].hostname;
-						}
-					}
-					respond(resultSet);
-				});	
-			});
-			return true;
-		}.bind(this));
-	},
+ 				chrome.runtime.onMessage.addListener(function(request,sender, respond){
+ 					if(self.indexCount > 0){
+ 						return respond([{
+ 							title:"Fruumo is currently building it's index to serve your searches better",
+ 							url:"https://fruumo.com/indexing"
+ 						}]);
+ 					}
+ 					if(request.type != "history-search"){
+ 						return;
+ 					}
+ 					var query = request.data.query;
+ 					if(query[query.length-1] == " "){
+ 						return respond([]);
+ 					}
+ 					this.db.transaction(function(tx){
+ 						tx.executeSql('SELECT * FROM urls WHERE ( (hostname LIKE ?) OR (hostname LIKE ?) OR (title LIKE ?) ) ORDER BY frecency DESC LIMIT 4' ,['%'+query+'%','%'+query.replace('www.','')+'%','%'+query+'%'], function(tx, results){
+ 							var resultSet = [];
+ 							for(var i=0;i<=results.rows.length-1; i++){
+ 								resultSet.push(results.rows.item(i));
+ 								if(resultSet[i].title == ""){
+ 									resultSet[i].title = resultSet[i].hostname;
+ 								}
+ 							}
+ 							respond(resultSet);
+ 						}, function(){
+ 							self.buildIndex();
+ 							return respond([{
+ 								title:"Fruumo is currently building it's index to serve your searches better",
+ 								url:"https://fruumo.com/indexing"
+ 							}]);
+ 						});	
+ 					});
+
+ 					return true;
+ 				}.bind(this));
+
+ 				chrome.bookmarks.onCreated.addListener(function(id, bookmark){
+ 					this.indexBookmarks(bookmark);
+ 				}.bind(this));
+ 				chrome.bookmarks.onRemoved.addListener(function(id,removeInfo){
+ 					console.log(id);
+ 					this.removeItemFromIndex({
+ 						id:id
+ 					});
+ 				}.bind(this));
+ 			},
 	//Assumes DB is open
 	buildIndex:function(){
 		var self = this;
 		chrome.history.search({text:"", startTime:new Date().getTime()-1209600000, maxResults:50000}, function(history){
 			console.log("Adding to Autocomplete Database:" + history.length);
+			self.indexCount = history.length-2;
 			for(var i in history){
 				historyItem = history[i];
 				if(historyItem.url.indexOf("chrome://")!= -1 || historyItem.url.indexOf("chrome-extension://")!= -1)
 					continue;
-				self.addItemToIndex(historyItem);
+				self.addItemToIndex(historyItem, function(){
+					self.indexCount--;
+				});
 			}
 		});
 		chrome.bookmarks.getTree(function(tree){
@@ -94,41 +130,56 @@ module.exports = {
 				this.indexBookmarks(tree[i]);
 		}.bind(this));
 	},
-	addItemToIndex:function(historyItem){
+	addItemToIndex:function(historyItem, callback){
+		var cb = function(){}
+		if(callback){
+			cb = callback;
+		}
 		self = this;
-		chrome.history.getVisits({url:historyItem.url}, function(visitItems){
-			visitItems.reverse();
-			var extractor = document.createElement('a');
-			if(!this.type){
-				extractor.href = this.url;
-				this.url = extractor.protocol+"//"+extractor.hostname.replace(/\/$/, "");
-				this.hostname = extractor.hostname.replace(/\/$/, "");
-				this.hostname = this.hostname.replace('www.','');
-			} else if(this.type == 2){
-				this.hostname = this.url.replace('http://','').replace('https://','');
-			}
-			self.db.transaction(function(tx){
-				//tx.executeSql('IF EXISTS(SELECT hostname FROM urls WHERE hostname = ?) UPDATE ',[this.hostname])
-				this.frecency = self.score(visitItems, this.type);
-				if(this.title == "")
-					this.title = this.hostname;
-				tx.executeSql('UPDATE urls SET frecency=frecency+?,title=? WHERE hostname=?' ,[this.frecency,this.title, this.hostname], function(tx, results){
-					if(results.rowsAffected < 1){
-						tx.executeSql('INSERT INTO urls (type, url, hostname, title,frecency) VALUES (?, ?,?, ?,?)', [this.type?this.type:1, this.url,this.hostname,this.title,this.frecency],function(){}, console.log);
-					}
-				}.bind(this),console.log);
+		this.openDb(function(){
+			chrome.history.getVisits({url:this.url}, function(visitItems){
+				visitItems.reverse();
+				var extractor = document.createElement('a');
+				if(!this.type){
+					extractor.href = this.url;
+					this.url = extractor.protocol+"//"+extractor.hostname.replace(/\/$/, "");
+					this.hostname = extractor.hostname.replace(/\/$/, "");
+					this.hostname = this.hostname.replace('www.','');
+				} else if(this.type == 2){
+					this.hostname = this.url.replace('http://','').replace('https://','');
+				}
+				self.db.transaction(function(tx){
+					//tx.executeSql('IF EXISTS(SELECT hostname FROM urls WHERE hostname = ?) UPDATE ',[this.hostname])
+					this.frecency = self.score(visitItems, this.type);
+					cb();
+					if(this.title == "")
+						this.title = this.hostname;
+					tx.executeSql('UPDATE urls SET frecency=frecency+?,title=? WHERE hostname=?' ,[this.frecency,this.title, this.hostname], function(tx, results){					
+						if(results.rowsAffected < 1){
+							tx.executeSql('INSERT INTO urls (id,type, url, hostname, title,frecency) VALUES (?,?, ?,?, ?,?)', [this.rId?this.rId:null,this.type?this.type:1, this.url,this.hostname,this.title,this.frecency],function(){}, console.log);
+						}
+					}.bind(this),console.log);
+				}.bind(this));
 			}.bind(this));
-		}.bind(historyItem));
+		}.bind(historyItem),true);
+	},
+	removeItemFromIndex: function(removeInfo){
+		if(removeInfo.id){
+			return this.db.transaction(function(tx){
+				tx.executeSql('DELETE FROM urls WHERE id=? ',[removeInfo.id], function(){}, function(){});
+			});
+		}
+		if(removeInfo.hostname){
+			return this.db.transaction(function(tx){
+				tx.executeSql('DELETE FROM urls WHERE hostname=?',[removeInfo.hostname], function(){}, function(){});
+			});	
+		}
 	},
 	indexBookmarks: function(tree){
 		//base case
 		if(tree.url){
-			/*this.idx.addDoc({
-				id:'bookmark:'+tree.id,
-				title:tree.title,
-				subtitle:tree.url
-			});*/
 			this.addItemToIndex({
+				rId:tree.id,
 				url:tree.url,
 				title:tree.title,
 				type:2
