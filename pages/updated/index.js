6d92b456c9ba2eacd573c03d50c3d8ad0170b0f8
Manak <manak.kapoor@gmail.com>
2018-06-06 19:02:06 -0400

    Updated updates page to look nicer

--- pages/updated/index.js
+++ pages/updated/index.js
@@ -2,16 +2,17 @@ require('./index.scss');
 
 document.getElementById("app-version").innerText = chrome.app.getDetails().version;
 document.getElementById("username").innerText = (localStorage.username == "" || !localStorage.username? " there": localStorage.username);
-/*var updateList = document.getElementById("update-list");
-//var updates = JSON.parse(localStorage.updateScreen);
-/*for(var i in updates){
+
+var updateList = document.getElementById("update-list");
+var updates = JSON.parse(localStorage.updateScreen);
+for(var i in updates){
 	var li = document.createElement('li');
 	li.innerText = updates[i];
 	updateList.appendChild(li);
 }
-*/
-/*
+
+
 document.getElementById("display-changes").addEventListener("click", function(e){
-	this.style.display = "none";
+	//this.style.display = "none";
 	document.getElementById("update-list").style.display = "block";
-});*/
+});
